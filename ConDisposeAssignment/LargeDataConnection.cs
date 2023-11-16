d1621aedd92500804cf9ba0e22a739ae9d848925
Maheshvema <babumahesh2088@gmail.com>
2023-11-16 16:07:03 +0530

    modified

---
+++ ConDisposeAssignment/LargeDataConnection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConDisposeAssignment
+{
+    public class LargeDataCollection : IDisposable
+    {
+        private List<object> data;
+
+        public LargeDataCollection(IEnumerable<object> initialData)
+        {
+            data = new List<object>(initialData);
+        }
+
+        public void Add(object item)
+        {
+            data.Add(item);
+        }
+
+        public bool Remove(object item)
+        {
+            return data.Remove(item);
+        }
+
+        public object this[int index]
+        {
+            get { return data[index]; }
+            set { data[index] = value; }
+        }
+
+        public void Dispose()
+        {
+            if (data != null)
+            {
+                foreach (var item in data)
+                {
+                    (item as IDisposable)?.Dispose();
+                }
+                data = null;
+            }
+        }
+    }
+}
---
+++ ConDisposeAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConDisposeAssignment
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            LargeDataCollection dataCollection = new LargeDataCollection(new object[] { 1, "hello", new object() });
+
+            dataCollection.Add(2);
+            dataCollection.Add("world");
+            dataCollection.Add(new object());
+
+            Console.WriteLine(dataCollection[0]); // 1
+            Console.WriteLine(dataCollection[1]); // "hello"
+            Console.WriteLine(dataCollection[2]); // object instance
+
+            dataCollection.Remove(1);
+            dataCollection.Remove("world");
+            dataCollection.Remove(new object());
+
+            dataCollection.Dispose();
+            Console.ReadKey();
+        }
+    }
+}
